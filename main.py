7f499833a724017005869b12fffef65c2e430008
pwrply <homarsimpson90@gmail.com>
2026-04-28 22:45:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title="Löwen Frankfurt News")
 
 @app.get("/")
 def root():
-    return {"status": "ok"}
+    return {
+        "app": "Löwen Frankfurt News",
+        "message": "Backend läuft!"
+    }
+
+@app.get("/hello")
+def hello():
+    return {"hello": "Willkommen 🦁🏒"}
