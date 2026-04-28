7a23ad6eeb113b29dee8a7e1d366b4126d42f5aa
pwrply <homarsimpson90@gmail.com>
2026-04-28 22:50:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ def root():
 @app.get("/hello")
 def hello():
     return {"hello": "Willkommen 🦁🏒"}
+import os
+import psycopg2
+
+@app.get("/db-test")
+def db_test():
+    try:
+        conn = psycopg2.connect(os.environ["DATABASE_URL"])
+        conn.close()
+        return {"database": "connected ✅"}
+    except Exception as e:
+        return {"database": "error ❌", "detail": str(e)}
